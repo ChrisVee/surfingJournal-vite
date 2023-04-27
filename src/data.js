928e05af73cdad9cab6018b24640efd4018d2066
Chris Viglietta <CSViglietta@gmail.com>
2023-04-27 17:05:54 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        title: "Fuerteventura",
+        location: "Spain",
+        googleMapsUrl: "https://goo.gl/maps/UTCDFeDHMt8XqUGU9",
+        travelDate: "October - March",
+        description: "Fuerteventura, the second largest island in the Canary Islands of Spain, lies in the Atlantic Ocean, 100 km from the northern coast of Africa. It is primarily known as a vacation destination due to its white sand beaches and year-round warm temperatures tempered by constant winds. The many beaches that surround it are separated by cliffs and sheltered coves.",
+        imageUrl: "https://images.unsplash.com/photo-1658844496048-cf606aaff0a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "Santa Teresa",
+        location: "Costa Rica",
+        googleMapsUrl: "https://goo.gl/maps/davLPSM3zeQfngvaA",
+        travelDate: "April - November",
+        description: "Costa Rica is a Central American country covered in tropical rainforest and located between the Caribbean Sea and the Pacific Ocean. Although its capital, San José, is home to cultural institutions like the Pre-Columbian Gold Museum, Costa Rica is known for its beaches, volcanoes and biodiversity.",
+        imageUrl: "https://images.unsplash.com/photo-1482531007909-192ac913980a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "Uluwatu",
+        location: "Bali",
+        googleMapsUrl: "https://goo.gl/maps/f7YfWGWiQHB27Rw18",
+        travelDate: "May - September",
+        description: `Uluwatu is an area located at the southwestern tip of the Bukit Peninsula in Bali, Indonesia. It houses the Pura Luhur Uluwatu temple. The name Uluwatu comes from ulu, which means "end of land", and watu, which means "rock".`,
+        imageUrl: "https://images.unsplash.com/photo-1569271532860-dd35503aaf1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "San Jose del Cabo",
+        location: "Mexico",
+        googleMapsUrl: "https://goo.gl/maps/zF7dRiqGsvoT8c2U8",
+        travelDate: "May-November",
+        description: `For a surfer looking to get lost for a few days, there’s a lot to love about Cabo San Lucas. An easy airport less than 30 minutes from the sand, warm water and some epic setups make a trip to Cabo San Lucas an easy call to make when the conditions come together. The best part of surf tripping to Cabo San Lucas is that there is no shortage of options. Stay in a luxury resort within the city, get comfortable, enjoy the sights, surf Zippers, it’s all good.`,
+        imageUrl: "https://images.unsplash.com/photo-1416163255873-f17745e8f851?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "Puaena Point",
+        location: "Hawaii",
+        googleMapsUrl: "https://goo.gl/maps/xUbw3uPcbbxJQH277",
+        travelDate: "November-January",
+        description: `Puaena Point in Oahu is an exposed reef break that only works when conditions are just right. Summer in particular tends to be flat. Works best in offshore winds from the southeast. Groundswells more frequent than windswells and the best swell direction is from the northwest. A reef breaks right. Often Crowded. Beware of urchins, rocks and pollution.`,
+        imageUrl: "https://images.unsplash.com/photo-1523606772308-64a28db0ef2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "Biarritz",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/svMhrVScaK2Sy6qd6",
+        travelDate: "May, June, Sepetember, October",
+        description: `With surf spot after surf spot, rights and lefts, steep and powerful, mellow and long, hollow and rippable waves, you are spoilt for choice in Southwest France. This is the home of Biarritz, the birthplace of European surfing. It’s also where you’ll find the best places to learn to surf in France.`,
+        imageUrl: "https://images.unsplash.com/photo-1527731149372-fae504a1185f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    },
+    {
+        title: "Malibu",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/LMCD5gLGUjQDXg3BA",
+        travelDate: "June - October",
+        description: `With its expansive beaches, turquoise waters, salty air, and perfect waves, there is no better surfing destination in the Los Angeles area than in Malibu. There’s something for every level of surfer, whether you’ve never stepped foot on a board or you’re training for a competition.`,
+        imageUrl: "https://images.unsplash.com/photo-1568418268058-c664ee87d35d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&h=1170&q=80"
+    }
+]
