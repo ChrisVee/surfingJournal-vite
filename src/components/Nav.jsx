02937e72025243a15d15ca36c239005ba4393af2
Chris Viglietta <CSViglietta@gmail.com>
2023-04-27 17:09:21 +0200

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Card from "./Card"
 
 export default function Nav () {
     return (
         <div className="nav--container">
-            <img src="./images/man.png" className="nav--logo" width="30px"/>
+            <img src="/images/man.png" className="nav--logo" width="30px"/>
             <h3 className="nav--title">My Surf Adventures</h3>
         </div>
     )
